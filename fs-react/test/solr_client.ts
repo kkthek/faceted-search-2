eb310491d4ff7aeb467639f9c1ad52c41255932a
Kai Kühn <kuehn@diqa-pm.com>
2024-07-09 21:23:28 +0200

    fixed tests

--- fs-react/test/solr_client.ts
+++ fs-react/test/solr_client.ts
@@ -18,17 +18,17 @@ let assert = require('assert');
 
 describe('getParams()', function () {
     it('search text one word', function () {
-        let params = initSolr().getParams("test", [], [], [], [], "");
+        let params = initSolr().getParams("test", [], [], [], [], [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
 describe('getParams()', function () {
     it('search text 2 words', function () {
-        let params = initSolr().getParams("test test2", [], [], [], [], "");
+        let params = initSolr().getParams("test test2", [], [], [], [], [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test+test2&rows=10&sort=&wt=json&q.alt=smwh_search_field%3A%28%2Btest+AND+%2Btest2*+%29+OR+smwh_search_field%3A%28test+AND+test2%29+OR+smwh_title%3A%28test+AND+test2%29+OR+smwh_displaytitle%3A%28test+AND+test2%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test+test2&rows=10&sort=&wt=json&q.alt=smwh_search_field%3A%28%2Btest+AND+%2Btest2*+%29+OR+smwh_search_field%3A%28test+AND+test2%29+OR+smwh_title%3A%28test+AND+test2%29+OR+smwh_displaytitle%3A%28test+AND+test2%29');
     });
 });
 
@@ -38,9 +38,9 @@ describe('getParams()', function () {
         let attributeFacets = [
             {property: {title: 'HasChild', type: Datatype.string}, value: 'Thomas'}
         ];
-        let params = initSolr().getParams("test", attributeFacets, [], [], [], "");
+        let params = initSolr().getParams("test", attributeFacets, [], [], [], [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_attributes%3Asmwh_HasChild_xsdvalue_t&fq=smwh_HasChild_s%3A%22Thomas%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_attributes%3Asmwh_HasChild_xsdvalue_t&fq=smwh_HasChild_s%3A%22Thomas%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
@@ -50,9 +50,9 @@ describe('getParams()', function () {
             {property: {title: 'HasChild', type: Datatype.string}, value: 'Thomas' },
             {property: {title: 'HasChild', type: Datatype.string}, value: 'Markus' }
         ];
-        let params = initSolr().getParams("test", attributeFacets, [], [], [], "");
+        let params = initSolr().getParams("test", attributeFacets, [], [], [], [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_attributes%3Asmwh_HasChild_xsdvalue_t&fq=smwh_HasChild_s%3A%22Thomas%22&fq=smwh_HasChild_s%3A%22Markus%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_attributes%3Asmwh_HasChild_xsdvalue_t&fq=smwh_HasChild_s%3A%22Thomas%22&fq=smwh_HasChild_s%3A%22Markus%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
@@ -61,9 +61,9 @@ describe('getParams()', function () {
         let propertyFacets = [
             {property: {title: 'IsInCity', type: Datatype.wikipage}, value:  { title: 'Munich', displayTitle: 'München'} }
         ];
-        let params = initSolr().getParams("test", propertyFacets, [], [], [], "");
+        let params = initSolr().getParams("test", propertyFacets, [], [], [], [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_properties%3Asmwh_IsInCity_t&fq=smwh_IsInCity_s%3A%22Munich%7CM%C3%BCnchen%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_properties%3Asmwh_IsInCity_t&fq=smwh_IsInCity_s%3A%22Munich%7CM%C3%BCnchen%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
@@ -73,18 +73,18 @@ describe('getParams()', function () {
             {property: {title: 'IsInCity', type: Datatype.wikipage}, value:  { title: 'Munich', displayTitle: 'München'} },
             {property: {title: 'IsInCity', type: Datatype.wikipage}, value:  { title: 'Frankfurt', displayTitle: 'Frankfurt'} }
         ];
-        let params = initSolr().getParams("test", propertyFacets, [], [], [], "");
+        let params = initSolr().getParams("test", propertyFacets, [], [], [], [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_properties%3Asmwh_IsInCity_t&fq=smwh_IsInCity_s%3A%22Munich%7CM%C3%BCnchen%22&fq=smwh_IsInCity_s%3A%22Frankfurt%7CFrankfurt%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_properties%3Asmwh_IsInCity_t&fq=smwh_IsInCity_s%3A%22Munich%7CM%C3%BCnchen%22&fq=smwh_IsInCity_s%3A%22Frankfurt%7CFrankfurt%22&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
 describe('getParams()', function () {
     it('categoryFacets', function () {
         let categoryFacets = ['Country', 'Western country'];
-        let params = initSolr().getParams("test", [], categoryFacets, [], [], "");
+        let params = initSolr().getParams("test", [], [], categoryFacets, [], [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_categories%3ACountry&fq=smwh_categories%3AWestern_country&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_categories%3ACountry&fq=smwh_categories%3AWestern_country&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
@@ -92,18 +92,18 @@ describe('getParams()', function () {
 describe('getParams()', function () {
     it('namespaceFacets', function () {
         let namespaceFacets = [14, 10];
-        let params = initSolr().getParams("test", [], [], namespaceFacets, [], "");
+        let params = initSolr().getParams("test", [], [], [], namespaceFacets, [], "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_namespace_id%3A14&fq=smwh_namespace_id%3A10&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&fq=smwh_namespace_id%3A14&fq=smwh_namespace_id%3A10&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
 describe('getParams()', function () {
     it('extraProperties', function () {
         let extraProperties = [{title: 'Has Name', type: Datatype.string}];
-        let params = initSolr().getParams("test", [], [], [], extraProperties, "");
+        let params = initSolr().getParams("test", [], [], [], [], extraProperties, "", null, []);
         console.log(params.toString());
-        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cscore%2Csmwh_displaytitle%2Csmwh_Has__Name_xsdvalue_t&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
+        assert.equal(params.toString(), 'defType=edismax&boost=max%28smwh_boost_dummy%29&facet=true&facet.field=smwh_categories&facet.field=smwh_attributes&facet.field=smwh_properties&facet.field=smwh_namespace_id&facet.mincount=1&json.nl=map&fl=smwh__MDAT_datevalue_l%2Csmwh_categories%2Csmwh_directcategories%2Csmwh_attributes%2Csmwh_properties%2Csmwh_title%2Csmwh_namespace_id%2Cid%2Cscore%2Csmwh_displaytitle%2Csmwh_Has__Name_xsdvalue_t&hl=true&hl.fl=smwh_search_field&hl.simple-pre=%3Cb%3E&hl.simple-post=%3C%2Fb%3E&hl.fragsize=250&searchText=test&rows=10&sort=&wt=json&q.alt=smwh_search_field%3A%28%2Btest*+%29+OR+smwh_search_field%3A%28test%29+OR+smwh_title%3A%28test%29+OR+smwh_displaytitle%3A%28test%29');
     });
 });
 
