015c1cb877aabf81aa7c23a5801c1386d99c69d3
Kai Kühn <kuehn@diqa-pm.com>
2025-04-11 19:29:22 +0200

    wiki endpoint for document-by-id

---
+++ src/Endpoints/DocumentByIdEndpoint.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DIQA\FacetedSearch2\Endpoints;
+
+use DIQA\FacetedSearch2\Model\Request\DocumentByIdQuery;
+use DIQA\FacetedSearch2\Setup;
+use MediaWiki\Rest\Handler;
+use MediaWiki\Rest\Response;
+
+class DocumentByIdEndpoint extends Handler
+{
+
+    public function execute()
+    {
+        $solrClient = Setup::getFacetedSearchClient();
+        $jsonBody = $this->getRequest()->getBody();
+        $documentQuery = DocumentByIdQuery::fromJson($jsonBody);
+        $response = $solrClient->requestDocument($documentQuery->getId());
+        $r = new Response(json_encode($response));
+        $r->setHeader('Content-Type', 'application/json');
+        return $r;
+    }
+
+}
