220cecbd895799a1113f1e4c234c558671297561
Kai Kühn <kuehn@diqa-pm.com>
2025-09-26 10:41:53 +0200

    fix bug in numeric clusterer

--- test/Utils/NumericClustererTest.php
+++ test/Utils/NumericClustererTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 final class NumericClustererTest extends TestCase
 {
 
-    public function testSmth()
+    public function testIntegers()
     {
         $clusterer = new NumericClusterer(true);
         $clusters = $clusterer->makeClusters(50, 500, 10);
@@ -18,6 +18,30 @@ final class NumericClustererTest extends TestCase
 
         $this->assertEquals("455", $clusters[9]->from);
         $this->assertEquals("500", $clusters[9]->to);
+
+    }
+
+    public function testFloatValues()
+    {
+        $clusterer = new NumericClusterer(false);
+        $clusters = $clusterer->makeClusters(1296, 1296.9, 10);
+
+        $this->assertEquals("1296", $clusters[0]->from);
+        $this->assertEquals("1296.09", $clusters[0]->to);
+
+        $this->assertEquals("1296.81", $clusters[9]->from);
+        $this->assertEquals("1296.9", $clusters[9]->to);
+
+    }
+
+    public function testFloatValuesSameMinMax()
+    {
+        $clusterer = new NumericClusterer(false);
+        $clusters = $clusterer->makeClusters(145, 145, 10);
+
+        $this->assertCount(1, $clusters);
+        $this->assertEquals("145", $clusters[0]->from);
+        $this->assertEquals("145", $clusters[0]->to);
 
     }
 }
