42929101c5741626a8cdcf6772736ee9935f3a7f
Kai Kühn <kuehn@diqa-pm.com>
2025-11-28 18:12:10 +0100

    sort option or-dialog

--- fs-react/src/common/datatypes.ts
+++ fs-react/src/common/datatypes.ts
@@ -444,7 +444,7 @@ export class PropertyWithURL extends Property implements ElementWithURL {
 }
 
 @jsonObject
-export class ValueCount {
+export class ValueCount implements Sortable<ValueCount> {
     @jsonMember({deserializer: value => ValueDeserializer.deserializeValue(value)})
     value: string | number | Date | null;
     @jsonMember(MWTitleWithURL)
@@ -454,13 +454,16 @@ export class ValueCount {
     @jsonMember(Number)
     count: number;
 
-    compare(valueCount: ValueCount) {
-        if (this.value && valueCount.value) {
-            return this.value.toLocaleString().localeCompare(valueCount.value.toLocaleString());
-        } else if (this.mwTitle && valueCount.mwTitle) {
-            return this.mwTitle.title.toLocaleString().localeCompare(valueCount.mwTitle.title.toLocaleString());
+    compareAlphabetically(that: ValueCount): number {
+        if (this.value && that.value) {
+            return this.value.toLocaleString().localeCompare(that.value.toLocaleString());
+        } else if (this.mwTitle && that.mwTitle) {
+            return this.mwTitle.displayTitle.toLocaleString().localeCompare(that.mwTitle.displayTitle.toLocaleString());
         }
         return 0;
+    }
+    compareByCount(that: ValueCount): number {
+        return that.count - this.count;
     }
 
     compareToSelectedFirst(other: ValueCount, propertyFacet: PropertyFacet) {
